82bbb9b124c86fa1fd24de060c2b24ec03c29c1b
EternalEdward <72557047+EternalEdward@users.noreply.github.com>
2021-06-13 00:22:41 +0800

    Update QAQ.cpp

--- QAQ.cpp
+++ QAQ.cpp
@@ -6,45 +6,63 @@ using namespace std;
    程序可以统计出文字、数字、空格的个数；
    静态存储一页文章，每行最多不超过80个字符，共N行。
 */
-typedef struct Chunk{
-    char ch[CH];
-    struct Chunk *next;
-}Chunk;
+typedef struct Node{
+    char ch[2];
+    struct Node *next;
+}Node;
 
 typedef struct List{
-	int curlen;
-    Chunk *head;
-	Chunk *tail;
-}LString;
+	int length;
+    Node data[81];
+    Node *head;
+	Node *tail;
+}LString,*LinkLString;
 
-void input(Chunk &S,int n){
-     for(int i=1;i<=n;i++){
-        cin>>S.ch;
-         S=S.next;
+void input(LString &S,int n){
+    *LinkLString p=S;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=80;j++)
+            cin>>
     }
-   
 }
 
-void count(Chunk S){//分别统计出其中英文字母数和空格数及整篇文章总字数；
-
-}
-
-void compare(Chunk S,string ch){//用来统计某一字符串  在文章中出现的次数，并输出该次数；
+void count(LString S){//
 
 }
 
-void del(Chunk &S,string ch){//删除某一子串，并将后面的字符前移。
+void compare(LString S,string ch){//
 
 }
-void mune(){
-    printf("\t1）分别统计出其  中英文字母数和空格数及整篇文章总字数；/n\t2）统计某一字符串  在文章中出现的次数，并输出该次数；/n\t3）删除某一子串，并将后面的字符前移。/n");
+
+void del(LString &S,string ch){//
+
+}
+int mune(){
+    int x;
+    cout<<"1) 文本输入"<<endl;
+    cout<<"2）统计出其中英文字母数和空格数及整篇文章总字数；"<<endl;
+    cout<<"3）统计某一字符串  在文章中出现的次数，并输出该次数；/n\t"<<endl;
+    cout<<"4）删除某一子串，并将后面的字符前移。/n"<<endl;
+    cout<<"5）打印文本"<<endl;
+    cout<<"请做出你的选择：";
+    cin>>x;
+    return x;
 }
 
 int main(){
     LString S;
     int n;
-    cout<<"请输入要输入的文章的行数："<<endl;
-    cin>>n;
+    while(1){
+        switch(mune()){
+            case 1:{break;}
+            case 2:{break;}
+            case 3:{break;}
+            case 4:{break;}
+            case 5:{break;}
+            default:{cout<<"你有一个错误的输入请重新输入"<<endl;break;}
+        }
+    }
    
+
     return 0;
 }
